648de3122b2acfb8aa8ff1b9d516b1417cb08dab
Gustavo Ambiel <57497708+GuSSniper@users.noreply.github.com>
2021-03-10 17:33:04 -0300

    gugu homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+//https://training-next-gussniper.vercel.app/
+
 function Home() {
     return (
     <div>
         <h1>WebPage do GuGu</h1>
         <h3>com butão</h3>
         <br></br>
-        <div>clica ae.....         <button>BUTAO</button></div>
+        <div>só clicar!</div>         
+        <button>BUTAO</button>
     </div>
     )
 }
