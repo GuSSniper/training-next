10c86e908766656f5a9d617a92e91481f7a2be1a
Gustavo Ambiel <57497708+GuSSniper@users.noreply.github.com>
2021-03-10 18:07:35 -0300

    gugu homepage

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-//https://training-next-gussniper.vercel.app/
-// git add -A
-// git commit -m 'gugu homepage'
-// git push origin main
+/* comentarios:
+
+https://training-next-gussniper.vercel.app/
+git add -A
+ git commit -m 'gugu homepage'
+  git push origin main
+
+*/
 
 function Home() {
     return (
-    <div>
-        <h1> GuGu Deployment Training </h1>
-    </div>
-/*      
         <div>
+        <h1> GuGu Deployment Training </h1>
         <h2> Git | Vercel | Next </h2>
         <br></br>
-        </div>
-    <div>
+        <br></br>
         <h3>...óia um butão...</h3>
         <button>BUTAO</button>
     </div>
-*/
+
     )
 }
 
