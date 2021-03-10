3a8f68b4a92c1d3360775f56a9b6416a2b74883d
Gustavo Ambiel <57497708+GuSSniper@users.noreply.github.com>
2021-03-10 17:59:48 -0300

    gugu homepage

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,18 @@
 function Home() {
     return (
     <div>
-        <h1>WebPage do GuGu</h1>
-        <h3>...com butão...</h3>
+        <h1> GuGu Deployment Training </h1>
+    </div>
+/*      
+        <div>
+        <h2> Git | Vercel | Next </h2>
         <br></br>
+        </div>
+    <div>
+        <h3>...óia um butão...</h3>
         <button>BUTAO</button>
     </div>
+*/
     )
 }
 
