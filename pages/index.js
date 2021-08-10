df96a678073c51421b0f51b2c0cda42835589ab4
Gustavo Ambiel <57497708+GuSSniper@users.noreply.github.com>
2021-08-10 18:29:56 -0300

    gugu homepage

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ git add -A
 */
 function Home() {
     return (
-        <><div>
+      <div>
         <h1> GuGu Deployment Training </h1>
         <h2> Git | Vercel | Next </h2>
         <br></br>
         <h4>The SUMMER is coming!</h4>
         <br></br>
-      </div><div>
-          <img src="https://i.ibb.co/xSPhfcs/G-laserized-bg-ada.jpg"></img>
-        </div></>
+        <img src="https://i.ibb.co/xSPhfcs/G-laserized-bg-ada.jpg">
+          </img>
+      </div>
     )
 }
 export default Home
